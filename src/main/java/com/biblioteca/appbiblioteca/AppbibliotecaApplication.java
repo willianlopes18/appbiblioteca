d16a19ee6f6f5ac5723e182fc5dc18ee69d3e26a
willianlopes018 <willian.lopes@hotmail.com.br>
2019-05-16 22:14:46 -0300

    criando repositorio

---
+++ src/main/java/com/biblioteca/appbiblioteca/AppbibliotecaApplication.java
@@ -0,0 +1,15 @@
+package com.biblioteca.appbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class AppbibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbibliotecaApplication.class, args);
+	}
+
+}
